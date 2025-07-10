3626811523e3b152527e3bc9c01a0a91bca1ff5c
Lxapk <73869778+Lxapk@users.noreply.github.com>
2025-07-10 14:17:04 +0800

    更新 diy-part2-x86-64-router.sh

--- immortalwrt/diy-part2-x86-64-router.sh
+++ immortalwrt/diy-part2-x86-64-router.sh
@@ -265,8 +265,10 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 # 定时任务。重启、关机、重启网络、释放内存、系统清理、网络共享、关闭网络、自动检测断网重连、MWAN3负载均衡检测重连、自定义脚本等10多个功能
 config_package_add luci-app-taskplan
 config_package_add luci-lib-ipkg
+
 #设置向导
 #config_package_add luci-app-netwizard
+
 ## iStore 应用市场 只支持 x86_64 和 arm64 设备
 ##git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebrowser luci-app-ssr-mudb-server
 #git_sparse_clone main https://github.com/linkease/istore luci
